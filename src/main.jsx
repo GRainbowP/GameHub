4bc170068528bd96e10cb651cf13f4e802dd07d0
GRainbowP <uT_3775@zsp3zamosc.pl>
2025-03-31 13:02:43 +0200

    Added Chakra UI and configured it

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,5 @@
+import { ChakraProvider } from '@chakra-ui/react'
+import {Provider} from './components/ui/provider.jsx'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
@@ -5,6 +7,8 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider>
+      <App />
+    </Provider>
   </StrictMode>,
 )
