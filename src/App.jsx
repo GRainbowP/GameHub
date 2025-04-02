1a0a97d9975e21e59c879ef74399cd2d73f4adfc
GRainbowP <uT_3775@zsp3zamosc.pl>
2025-04-02 09:09:56 +0200

    Basic structure using grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,54 @@
-import { Button } from '@chakra-ui/react'
+import { Grid, GridItem } from '@chakra-ui/react'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <Button colorScheme="blue">Kliknij mnie</Button>
+      <Grid
+        templateAreas={`"nav nav"
+                        "main side"
+                        "main side"`}  
+        templateRows={'50px 1fr 50px'}
+        templateColumns={'1fr'}
+        gap="1"
+        p="4"
+        minHeight='100vh'
+      >
+        <GridItem 
+          pl="2"
+          bg="blue.700"
+          area={'nav'}
+          borderRadius="md"
+          display="flex"
+          m="1"
+          alignItems="center"
+        >
+          Wielka nawigacja
+        </GridItem>
+        <GridItem 
+          pl="2"
+          bg="red.700"
+          area={'main'}
+          borderRadius="md"
+          display="flex"
+          m="1"
+          alignItems="center"
+        >
+          Główna sekcja
+        </GridItem>
+        <GridItem 
+          pl="2"
+          bg="yellow.700"
+          area={'side'}
+          borderRadius="md"
+          display="flex"
+          m="1"
+          alignItems="center"
+        >
+          Boczny panel
+        </GridItem>
+      </Grid>
     </>
   )
 }
