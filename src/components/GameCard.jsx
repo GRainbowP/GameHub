739b55cd5e7334502988356e47aa64c5c540d9b9
GRainbowP <uT_3775@zsp3zamosc.pl>
2025-04-24 09:32:26 +0200

    Optimalize images loading

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -1,13 +1,14 @@
 import { Card, Image, Heading, HStack } from "@chakra-ui/react"
 import PlatformIconList from "./PlatformIconList"
 import CriticScore from "./CriticScore"
+import getCroppedImageUrl from "../services/image-url"
 
 function GameCard({ game }) {
 
 
     return (
         <Card.Root borderRadius="10px" overflow="hidden">
-            <Image src={game.background_image} alt={game.name} />
+            <Image src={getCroppedImageUrl(game.background_image)} alt={game.name} />
             <Card.Body>
                 <PlatformIconList platforms={game.parent_platforms} />
                 <HStack justifyContent="space-between">
